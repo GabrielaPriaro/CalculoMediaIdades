0672bffa847017b7b419c233fcb35b1c234c0325
Gabriela <gabriela.priaro@gmail.com>
2024-10-26 12:24:04 +0100

    Adicionar arquivos de projeto.

---
+++ CalculoMediaIdades/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculoMediaIdades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
